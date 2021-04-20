8f8c5a712655bf90301c8df5aebfad4509f57364
dbyr <david.michael.byrne17@gmail.com>
2021-04-20 21:50:34 +1000

    Add extra example for de-genericing

---
+++ examples/hide_generic.rs
@@ -0,0 +1,37 @@
+use rb_tree::RBQueue;
+
+struct ByReference {
+    queue: RBQueue<i32, &'static dyn Fn(&i32, &i32) -> std::cmp::Ordering>,
+}
+
+impl Default for ByReference {
+    fn default() -> Self{
+        Self {
+            queue: RBQueue::new(&|l: &i32, r:&i32| {
+                l.cmp(r)
+            })
+        }
+    }
+}
+
+struct Boxing {
+    queue: RBQueue<i32, Box<dyn Fn(&i32, &i32) -> std::cmp::Ordering>>,
+}
+
+impl Default for Boxing {
+    fn default() -> Self{
+        Self {
+            queue: RBQueue::new(Box::new(|l: &i32, r:&i32| {
+                l.cmp(r)
+            }))
+        }
+    }
+}
+
+fn main() {
+    let mut h = ByReference::default();
+    h.queue.insert(1i32);
+
+    let mut b = Boxing::default();
+    b.queue.insert(1i32);
+}
